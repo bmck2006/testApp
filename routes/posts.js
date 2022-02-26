77847b099c371b5a458c2a3f551a6a292ca3d48b
Brian McKeown <bmckeown2011@gmail.com>
2022-02-25 20:25:23 -0500

    added posts routes config

---
+++ routes/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET posts index /posts */
+router.get('/', function(req, res, next) {
+    res.send('INDEX /posts');
+  });
+
+/* GET posts new /posts/new */
+router.get('/new', function(req, res, next) {
+    res.send('NEW /posts/new');
+  });
+
+  /* POST posts create /posts */
+router.post('/', function(req, res, next) {
+    res.send('CREATE /posts/new');
+  });
+
+  /* GET posts show /posts/:id */
+router.get('/:id', function(req, res, next) {
+    res.send('SHOW /posts/:id');
+  });
+
+  /* GET posts edit /posts/:id/edit */
+router.get('/:id/edit', function(req, res, next) {
+    res.send('EDIT /posts/:id/edit');
+  });
+
+  /* PUT posts update /posts/:id */
+router.put('/:id', function(req, res, next) {
+    res.send('UPDATE /posts/:id');
+  });
+
+  /* DELETE posts destroy /posts/:id */
+router.delete('/:id', function(req, res, next) {
+    res.send('DELETE /posts/:id');
+  });
+  
+  module.exports = router;
