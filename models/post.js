2f67d55099aab968a844077fb25d7ff7f51ec6be
Brian McKeown <bmckeown2011@gmail.com>
2022-02-25 22:23:35 -0500

    add post model

--- models/post.js
+++ models/post.js
@@ -1,12 +1,24 @@
-/** 
-Post
-- title - string
-- price - string
-- description - string
-- images - array of strings
-- location - string
-- lat - number
-- long - number
-- author - object id (ref user)
-- reviews - array of objects
-*/
+const mongoose = req('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    images: [ String ],
+    location: String,
+    lat: Number,
+    lng: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]   
+});
+
+module.exports = mongoose.model('Post', PostSchema);
