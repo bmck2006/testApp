5d0e3ac227a3c5203339abb95b9ea274c65452be
Brian McKeown <bmckeown2011@gmail.com>
2022-02-25 21:45:04 -0500

    add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Test App - Home' });
+});
+
+/* GET /register */
+router.get('/register', function(req, res, next) {
+  res.send('GET /register');
+});
+
+/* POST /register */
+router.post('/register', function(req, res, next) {
+  res.send('POST /register');
+});
+
+/* GET /login */
+router.get('/login', function(req, res, next) {
+  res.send('GET /login');
+});
+
+/* POST /login */
+router.post('/login', function(req, res, next) {
+  res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/profile', function(req, res, next) {
+  res.send('GET /profile');
+});
+
+/* PUT /profile/:user_id */
+router.put('/profile/:user_id', function(req, res, next) {
+  res.send('PUT /profile/:user_id');
+});
+
+/* GET /forgot */
+router.get('/forgot', function(req, res, next) {
+  res.send('GET /forgot');
+});
+
+/* PUT /forgot */
+router.put('/forgot', function(req, res, next) {
+  res.send('PUT /forgot');
+});
+
+/* GET /reset/:token */
+router.get('/reset/:token', function(req, res, next) {
+  res.send('GET /reset/:token');
+});
+
+/* PUT /reset/:token */
+router.put('/reset/:token', function(req, res, next) {
+  res.send('PUT /reset/:token');
 });
 
 module.exports = router;
