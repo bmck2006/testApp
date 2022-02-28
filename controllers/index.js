1b4b4c9b8f901ef455665b5baca3701d3b7d38f5
Brian McKeown <bmckeown2011@gmail.com>
2022-02-28 15:19:26 -0500

    updated the user controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,12 @@ const User = require('../models/user');
 
 module.exports = {
     postRegister(req, res, next) {
-        User.register(new User({username: req.body.username}), req.body.password, function(err) {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        image: req.body.image
+      });
+        User.register(newUser, req.body.password, function(err) {
             if (err) {
               console.log('error while user register!', err);
               return next(err);
